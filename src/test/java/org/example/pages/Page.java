262c63be87373694c39ef58a87af2c0f9c3d0b84
CleveyP <clevelandplonsey@gmail.com>
2026-01-29 05:22:10 -0800

    add image validation to Page base class

--- src/test/java/org/example/pages/Page.java
+++ src/test/java/org/example/pages/Page.java
@@ -1,12 +1,15 @@
 package org.example.pages;
 
-import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 
 public abstract class Page {
@@ -38,4 +41,37 @@ public abstract class Page {
     public void waitForText(By locator, String expected){
         wait.until(ExpectedConditions.textToBe(locator, expected));
     }
+
+
+    public boolean imageUrlWorks(String url) {
+        System.out.println("YOOOOO" + url);
+        try {
+            HttpURLConnection connection =
+                    (HttpURLConnection) new URL(url).openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            int status = connection.getResponseCode();
+
+            return status == 200;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+
+    public void validateAllImages(List<WebElement> images) {
+
+        for(WebElement img : images){
+            String imageSource = img.getAttribute("src");
+            if(imageSource == null){
+                Assert.fail("Image source not found");
+            }
+            Assert.assertFalse(imageUrlWorks(imageSource));
+        }
+    }
+
 }
