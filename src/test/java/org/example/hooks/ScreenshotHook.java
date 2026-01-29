966ab509c62f75192c52c4ee1cd235b43f11f8ea
CleveyP <clevelandplonsey@gmail.com>
2026-01-29 05:22:56 -0800

    create screenshot hook

--- src/test/java/org/example/hooks/ScreenshotHook.java
+++ src/test/java/org/example/hooks/ScreenshotHook.java
@@ -1,4 +1,37 @@
 package org.example.hooks;
 
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
+import lombok.RequiredArgsConstructor;
+import org.example.spring.ScenarioContext;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+
+@RequiredArgsConstructor
 public class ScreenshotHook {
+
+    private final WebDriver driver;
+    private final ScenarioContext scenarioContext;
+
+
+    @AfterStep
+    public void screenshotOnFailure() {
+        Scenario scenario = scenarioContext.getScenario();
+        if (scenario.isFailed()) {
+
+            byte[] screenshot = ((TakesScreenshot) driver)
+                    .getScreenshotAs(OutputType.BYTES);
+
+            scenario.attach(
+                    screenshot,
+                    "image/png",
+                    "FAILED STEP Screenshot"
+            );
+        }
+    }
+
+
 }
+
