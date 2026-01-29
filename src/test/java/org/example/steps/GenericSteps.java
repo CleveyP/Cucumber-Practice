8dbe840574c5b0bc5b5764df7e21134392d1887a
CleveyP <clevelandplonsey@gmail.com>
2026-01-29 05:21:12 -0800

    add functionality to GenericSteps

--- src/test/java/org/example/steps/GenericSteps.java
+++ src/test/java/org/example/steps/GenericSteps.java
@@ -1,4 +1,38 @@
 package org.example.steps;
 
+import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+@RequiredArgsConstructor
 public class GenericSteps {
+
+    public final WebDriverWait wait;
+
+
+    public void clickLinkOrButton(String labelText) {
+
+        By locator = By.xpath(
+                ("//a[normalize-space()='%s'] | " +
+                        "//button[normalize-space()='%s']")
+                        .formatted(labelText, labelText)
+        );
+
+        WebElement element =
+                wait.until(ExpectedConditions.elementToBeClickable(locator));
+
+        assert element != null;
+        element.click();
+    }
+
+    public void  clickLinkOrButtonRepeatedly(String labelText, int times){
+        while(times>0){
+            clickLinkOrButton(labelText);
+            times--;
+        }
+    }
+
 }
